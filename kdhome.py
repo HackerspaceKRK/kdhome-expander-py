01821b11b11d7185439df8d392b2d2a79fbd4f6c
Jakub Kramarz <jakub@hackerspace-krk.pl>
2017-11-11 04:58:44 +0100

    kdhome.py: pep8

--- kdhome.py
+++ kdhome.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python2.7  
+#!/usr/bin/env python2.7
 import smbus
 import mosquitto
-from twisted.internet import reactor, task 
+from twisted.internet import reactor, task
 
 MQTT_SERVER = 'rudy.at.hskrk.pl'
 TOPIC_PREFIX = 'kdhome'
@@ -11,38 +11,44 @@ input_expanders = (0x38, 0x3b)
 output_expanders = (0x39, 0x3a)
 
 mqttc = mosquitto.Mosquitto("kdhome")
-mqttc.will_set("%s/dropped" % TOPIC_PREFIX , "Sorry, I seem to have died.")
+mqttc.will_set("%s/dropped" % TOPIC_PREFIX, "Sorry, I seem to have died.")
 mqttc.connect(MQTT_SERVER, 1883, 60, True)
 
 bus = smbus.SMBus(smbus_bus)
 
+
 def kdconvert(data):
     return data[:4] + data[4:][::-1]
+
 
 def read_expander(address):
     data = bus.read_byte(address)
     data = format(data, '08b')
     return kdconvert(data)
 
+
 def write_expander(address, data):
     data = kdconvert(data)
     data = int(data, 2)
     bus.write_byte(address, data)
+
 
 def get_inputs_state():
     ports = map(
         read_expander,
         input_expanders
     )
-    inputs = ''.join(ports) 
+    inputs = ''.join(ports)
     return inputs
+
 
 def set_inputs_state(state):
     print "desired state %s" % state
     write_expander(output_expanders[0], state[:8])
     write_expander(output_expanders[1], state[8:])
 
-def inputs_callback(): 
+
+def inputs_callback():
     new_state = get_inputs_state()
     if new_state != inputs_callback.last_state:
         for i in range(0, len(new_state)):
@@ -52,6 +58,7 @@ def inputs_callback():
                 mqttc.publish("%s/input/%d" % (TOPIC_PREFIX, 15-i), state)
         inputs_callback.last_state = new_state
 inputs_callback.last_state = '1' * 16
+
 
 def output_callback(client, userdata, message):
     try:
@@ -74,6 +81,7 @@ def output_callback(client, userdata, message):
         pass
 output_callback.last_state = [1, 1, 1, 1,    1, 1, 1, 1,    1, 1, 1, 1,   1, 1, 1, 1]
 
+
 mqttc.subscribe('%s/output/+' % TOPIC_PREFIX, qos=0)
 mqttc.on_message = output_callback
 
@@ -83,4 +91,3 @@ task.LoopingCall(mqttc.loop_misc).start(5)
 task.LoopingCall(inputs_callback).start(0.1)
 
 reactor.run()
-
